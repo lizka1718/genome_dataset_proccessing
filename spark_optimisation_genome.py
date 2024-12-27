cf5a302817cccf8ca94579cf622bff1162c5b1d3
lizi <lizzzzo2005@gmail.com>
2024-12-27 17:32:17 +0400

    changed again

--- spark_optimisation_genome.py
+++ spark_optimisation_genome.py
@@ -8,58 +8,71 @@ import re
 #OBJECTIVE I
 
 
+from pyspark.sql import SparkSession
+import re
+import logging
+
 def find_repeats(sequence):
-    return re.findall(r'((.)\2+)', sequence)
+    return re.findall(r'(.)\1+', sequence)
 
-sc = SparkContext()
-data = sc.textFile("s3a://retail-employees/data/final_genome.fasta")
+if __name__ == "__main__":
 
-repeats = data.flatMap(lambda x: [match[0] for match in find_repeats(x)]) \
-              .map(lambda x: (x, 1)) \
-              .reduceByKey(lambda a, b: a + b)
+    logging.basicConfig(level=logging.INFO)
+    
 
+    spark = SparkSession.builder \
+        .appName("FindRepeats") \
+        .getOrCreate()
+    data = spark.read.text("s3a://retail-employees/data/final_genome.fasta").rdd
 
-results = repeats.collect()
-for result in results:
-    print(result)
+    repeats = (data.flatMap(lambda row: find_repeats(row.value))  
+                     .map(lambda x: (x, 1))
+                     .reduceByKey(lambda a, b: a + b, numPartitions=8)) 
+
+    result = repeats.take(100) 
+
+    logging.info(f"RESULT: {result}")
+    for r in result:
+        print(r)
+    spark.stop()
 
 
 #OBJECTIVE II
 
 
 
-# def non_repeating_sequence(sequence: str) -> bool:
-#     for i in range(len(sequence) - 1):
-#         if sequence[i] == sequence[i + 1]:
-#             return True
-#     return False  
+def non_repeating_sequence(sequence: str) -> bool:
+    for i in range(len(sequence) - 1):
+        if sequence[i] == sequence[i + 1]:
+            return True
+    return False  
 
-# if __name__ == "__main__":
-#     """
-#         Non-repeating sequence finder
-#     """
-#     APP = "NonRepeatingSequence"
-#     logging.basicConfig(level=logging.INFO)
-#     start_time = time.time()
+if __name__ == "__main__":
+    """
+        Non-repeating sequence finder
+    """
+    APP = "NonRepeatingSequence"
+    logging.basicConfig(level=logging.INFO)
+    start_time = time.time()
     
-#     sc = SparkContext(appName=APP)
-#     spark = SparkSession(sc)
+    sc = SparkContext(appName=APP)
+    spark = SparkSession(sc)
     
 
-#     labs = spark.read.text("s3a://retail-employees/data/final_genome.fasta").rdd
+    labs = spark.read.text("s3a://retail-employees/data/final_genome.fasta").rdd
     
-#     filter_sequence = labs.filter(lambda x: not non_repeating_sequence(x[0])) \
-#                           .map(lambda x: (x[0], 1))
+    filter_sequence = labs.filter(lambda x: not non_repeating_sequence(x[0])) \
+                          .map(lambda x: (x[0], 1))
 
 
-#     result = filter_sequence.collect()
-#     logging.info(f"RESULT: {result}")
+    result = filter_sequence.collect()
+    logging.info(f"RESULT: {result}")
     
-#     for r in result:
-#         print(r)
+    for r in result:
+        print(r)
 
-#     end_time = time.time()
-#     execution_time = end_time - start_time
-#     logging.info(f"time: {execution_time} seconds")
+    end_time = time.time()
+    execution_time = end_time - start_time
+    logging.info(f"time: {execution_time} seconds")
 
-#     spark.stop()
+    spark.stop()
